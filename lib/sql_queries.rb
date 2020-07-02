5fae545dad7eb6dbdc12e63c211e55ff6412614c
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-07-01 21:40:55 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,12 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS difference
+  FROM projects
+  INNER JOIN pledges
+    ON projects.id = pledges.project_id
+  GROUP BY projects.title
+  HAVING difference >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
@@ -48,7 +53,7 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "SELECT projects.category, SUM(pledges.amount) 
   FROM projects 
-  JOIN pledges 
+  INNER JOIN pledges 
     ON projects.id = pledges.project_id 
   WHERE projects.category = 'books'"
 end
